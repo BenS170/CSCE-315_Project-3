f1429efe934d936e70c483564301fb3562834939
BenS170 <Ben@spence.vip>
2022-10-30 15:07:09 -0500

    added all files

---
+++ databasepg.js
@@ -0,0 +1,20 @@
+const {Client} = require('pg')
+
+const client = new Client({
+    host: "csce-315-db.engr.tamu.edu",
+    user: "csce315_910_spence",
+    port: 5432,
+    password: "Tippah2002!",
+    database: "csce315_910_3"
+})
+
+client.connect();
+
+client.query('Select * from teammembers', (err, res) => {
+    if(!err){
+        console.log(res.rows);
+    }else{
+        console.log(err.message);
+    }
+    client.end;
+})
