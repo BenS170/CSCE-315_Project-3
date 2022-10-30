ca05d06b1acb42958ae70ee8093d6211f2b8b834
Ben-S17 <17183+Ben-S17@users.noreply.github.tamu.edu>
2022-10-30 14:39:00 -0500

    Added back Files

---
+++ ServerGUI/ServerGUI.js
@@ -0,0 +1,46 @@
+const {Client} = require("pg");
+//const express = require("express");
+//const app = express();
+
+const client = new Client({
+  "user": "csce315_910_spence",
+  "host": "csce-315-db.engr.tamu.edu",
+  "database": "csce315_910_3",
+  "password": "Tippah2002!",
+  "port": 5432
+});
+
+start();
+
+async function start() {
+    await connect();
+    const todos = await readTodos();
+    console.log(todos);
+
+    // const successCreate = await createTodo("Go to trader joes")
+    // console.log('Creating was ${successCreate}')
+
+    // const successDelete = await deleteTodo(1)
+    // console.log('Deleting was ${successDelete}')
+}
+
+async function connect() {
+    try {
+        await client.connect();
+    }catch (e){
+        console.error(`Failed to connect ${e}`);
+    }
+}
+
+async function readTodos(){
+    try {
+        const results = await client.query("select * from teammembers");
+        return results.rows;
+    }catch (e){
+        return [];
+    }
+}
+
+
+  
+  
