95b9d23866a76c1f688463d8f93ae96f4d0b6b5e
hcraft3 <112505804+hcraft3@users.noreply.github.com>
2022-11-08 18:51:13 -0600

    commiting add, update, and delete though they do not work completely, but this doesn't mess up the other functions so it should be fine

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
+const { query } = require('express');
 const express = require('express');
+const { Pool } = require('pg');
+const dotenv = require('dotenv').config();
+
 const app = express();
 const port = 3000;
 
+const pool = new Pool({
+    user: process.env.PSQL_USER,
+    host: process.env.PSQL_HOST,
+    database: process.env.PSQL_DATABASE,
+    password: process.env.PSQL_PASSWORD,
+    port: process.env.PSQL_PORT,
+    ssl: {rejectUnauthorized: false}
+});
+
+process.on('SIGINT', function() {
+    pool.end();
+    console.log('Application successfully shutdown');
+    process.exit(0);
+});
+
 app.set("view engine", "ejs");
 app.use(express.static('public'));
+app.use(express.json({extended: true, limit: '1mb'}))
+
 
 app.get('/', function(req, res) {
     res.render('index');
@@ -23,4 +44,74 @@ app.get('/managergui', function(req, res) {
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get('/getMenu', (req, res) => {
+    menu_items = [];
+    pool
+        .query('select * from menu_items;')
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++){
+                menu_items.push(query_res.rows[i]);
+            }
+            data = { result : menu_items };
+            console.log("Query done");
+            res.json(data);
+        }
+    );
+});
+
+app.post('/addMenuItem', (req, res) => {
+    console.log("inside add Menu item");
+    const { menuID, menuName,menuPrice,menuIngredients, menuIngNum, menuType } = req.body;
+    console.log(req.body);
+  
+    console.log("before the query");
+    // Database Code here
+    pool
+        .query("INSERT INTO menu_items ( menu_id , item_name , item_price , num_ingredients, ingredient_list,type ) VALUES( " + menuID + ", '" + menuName + "', " + menuPrice + ", " + menuIngNum + ", " + menuIngredients + + ", '" + menuType + "');")
+        .then(query_res => {
+        // for (let i = 0; i < query_res.rowCount; i++){
+        //     console.log(query_res.rows[i]);
+        // }
+        console.log("item added");
+    })
+
+    res.status(200).json({ menuID, menuName,menuPrice,menuIngredients, menuIngNum, menuType });
+});
+
+app.post('/updateMenuItem', (req, res) => {
+    console.log("inside update Menu item");
+    const { ID, price } = req.body;
+    console.log(req.body);
+  
+    // Database Code here
+     pool
+        .query("UPDATE menu_items SET item_price= '" + price +"' WHERE menu_id= '" + ID +"';")
+        .then(query_res => {
+        // for (let i = 0; i < query_res.rowCount; i++){
+        //     console.log(query_res.rows[i]);
+        // }
+        alert("item updated");
+    })
+
+    res.status(200).json({ ID, price });
+});
+
+app.post('/deleteMenuItem', (req, res) => {
+    console.log("inside update Menu item");
+    const { menuID } = req.body;
+    console.log(req.body);
+  
+    // Database Code here
+    pool
+        .query("DELETE FROM menu_items WHERE menu_id= '" + menuID + "';")
+        .then(query_res => {
+        // for (let i = 0; i < query_res.rowCount; i++){
+        //     console.log(query_res.rows[i]);
+        // }
+        alert("item deleted");
+    })
+
+    res.status(200).json({ menuID });
 });
--- public/javascript/ManagerGUI.js
+++ public/javascript/ManagerGUI.js
@@ -1,58 +1,158 @@
 
+
 // menu functions
-function viewMenu(){
-    alert("view menu");
-}
+const viewMenuButton = document.getElementById("viewMenuButton");
 
-function addMenuItem(){
-    alert("add menu");
-}
+viewMenuButton.addEventListener('click', function(e) {
+    console.log('view menu was clicked');
+  
+    fetch('/getMenu', {method: 'GET'})
+        .then(function(response) {
+            if(response.ok) return response.json();
+            throw new Error('Request failed.');
+        })
+            .then(function(data) {
+            // TODO: Modify HTML using the information received from the database
+            content = document.getElementById("managerView");
+            
+            console.log(data.result);
+        })
+        .catch(function(error) {
+            console.log(error);
+    });
+});
 
-function updateMenu(){
-    alert("update menu");
+const addMenuItemButton = document.getElementById("addMenuItemButton");
 
-}
+addMenuItemButton.addEventListener('click', function(e) {
+  console.log('add menu was clicked');
+  const menuID = prompt("What is the menu ID?");
+  const menuName = prompt("What is the name of the item?");
+  const menuPrice = prompt("What is the new price");
+  const menuIngredients = prompt("What ingredients do you want for the new item?");
+  const menuIngNum = prompt("How many ingredients is in the new item");
+  const menuType = prompt("What type of item is this?");
 
-function deleteMenuItem(){
-    alert("delete menu");
+  console.log('finished add menu prompts');
+  
+  const data = {menuID, menuName,menuPrice, menuIngredients, menuIngNum, menuType};
 
-}
+  console.log("after data");
+  
+  fetch('/addMenuItem', {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(function(response) {
+      if(response.ok) {
+        console.log('Click was recorded');
+        return;
+      }
+      throw new Error('Request failed.');
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+
+
+const  updateMenuItemButton = document.getElementById("updateMenuItemButton");
+
+updateMenuItemButton.addEventListener('click', function(e) {
+  console.log('update menu was clicked');
+  const menuID = prompt("What is the menu ID?");
+  const menuPrice = prompt("What is the new price");
+ 
+  
+  const data = {menuID,menuPrice};
+
+  fetch('/updateMenuItem', {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(function(response) {
+      if(response.ok) {
+        console.log('Click was recorded');
+        return;
+      }
+      throw new Error('Request failed.');
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+
+
+const  deleteMenuItemButton = document.getElementById("deleteMenuItemButton");
+
+deleteMenuItemButton.addEventListener('click', function(e) {
+  console.log('menu delete was clicked');
+  const menuID = prompt("What is the menu ID?"); 
+  
+  const data = {menuID};
+
+  fetch('/deleteMenuItem', {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(function(response) {
+      if(response.ok) {
+        console.log('Click was recorded');
+        return;
+      }
+      throw new Error('Request failed.');
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+
+
 
 
 // inventory functions
-function viewInventory(){
+async function viewInventory(){
     alert("view inventory");
 
 }
 
-function orderInventory(){
+async function orderInventory(){
     alert("order inventory");
 
 }
 
-function addInventoryItem(){
+async function addInventoryItem(){
     alert("add inventory");
 
 }
 
 
 // manager reports
-function salesReport(){
+async function salesReport(){
     alert("sales report");
 
 }
 
-function popularMenuReport(){
+async function popularMenuReport(){
     alert("popular report");
 
 }
 
-function excessReport(){
+async function excessReport(){
     alert("excess report");
 
 }
 
-function restockReport(){
+async function restockReport(){
     alert("restock report");
 
 }
