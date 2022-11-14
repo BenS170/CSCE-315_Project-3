155d0a0447f329fa1b46018720919d89bb95832e
Octavio Almanza <oa5967@tamu.edu>
2022-11-14 09:17:51 -0600

    Sales report feature works and date input has been simplified

--- public/javascript/ManagerGUI.js
+++ public/javascript/ManagerGUI.js
@@ -10,7 +10,7 @@ function makeMenuTable(data){
 
     for (let i = 0; i < data.result.length; i++){
         htmlMenuTable = htmlMenuTable + '<tr id = "menuItem">';
-        htmlMenuTable = htmlMenuTable + "<td>" + data.result[i].menu_id + "</td>";
+        htmlMenuTable = htmlMenuTable + '<td contentEditable="true">'+ data.result[i].menu_id + "</td>";
         htmlMenuTable = htmlMenuTable + "<td>" + data.result[i].item_name + "</td>";
         htmlMenuTable = htmlMenuTable + "<td>" + data.result[i].item_price + "</td>";
         htmlMenuTable = htmlMenuTable + "<td>" + data.result[i].num_ingredients + "</td>";
@@ -150,7 +150,6 @@ deleteMenuItemButton.addEventListener('click', function(e) {
 });
 
 
-
 function makeInventoryTable(data){
     htmlMenuTable = '<table> <tr id = "titleRow">';
     htmlMenuTable = htmlMenuTable + "<th>Item ID</th>";
@@ -196,37 +195,127 @@ viewInventoryButton.addEventListener('click', function(e) {
         .catch(function(error) {
             console.log(error);
     });
-})
+});
 
 // inventory functions
 async function orderInventory(){
     alert("order inventory");
-
 }
 
 async function addInventoryItem(){
     alert("add inventory");
 
 }
-
-
 // manager reports
-async function salesReport(){
-    alert("sales report");
 
+// Helper function: 
+
+/* 
+IMPORTANT: dateSelectors has a value: 
+    0 - NULL
+    1 - Sales report
+*/
+SALES = 1;
+
+function getStartDate(){
+    month = document.getElementById("startMonth");
+    day = document.getElementById("startDay");
+    year = document.getElementById("startYear");
+    return year.options[year.selectedIndex].text + "-" + month.value + "-" + day.options[day.selectedIndex].text;
 }
 
-async function popularMenuReport(){
-    alert("popular report");
-
+function getEndDate(){
+    month = document.getElementById("endMonth");
+    day = document.getElementById("endDay");
+    year = document.getElementById("endYear");
+    return year.options[year.selectedIndex].text + "-" + month.value + "-" + day.options[day.selectedIndex].text;
 }
 
-async function excessReport(){
-    alert("excess report");
+//Sales Report - Given a time window, display the sales by item from the order history
 
+const createSalesReport = document.getElementById("salesReportButton");
+createSalesReport.addEventListener('click', function(e) {
+    console.log('Sales Report Button was clicked');
+    document.getElementById("managerView").hidden = true;
+    document.getElementById("dateSelectors").hidden = false;
+    document.getElementById("dateSelectors").value = SALES;
+});
+
+const submitDate_SALES = document.getElementById("submitDates");
+submitDate_SALES.addEventListener('click', function(e) {
+    // IMPORTANT: Check if the "Submit Dates" button was clicked 
+    // because the manager wanted a sales report:
+    if (document.getElementById("dateSelectors").value != SALES){
+        return;
+    }
+
+    startDate = getStartDate();
+    endDate = getEndDate();
+
+    document.getElementById("dateSelectors").hidden = true;
+    document.getElementById("managerView").hidden = false;
+    salesReportLogic(startDate, endDate);
+});
+
+function salesReportLogic(startDate, endDate){
+    // SALES QUERY:
+    console.log(startDate + " to " + endDate);
+    const data = {startDate, endDate};
+  
+    fetch('/getSalesRep', {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    }).then(function(response) {
+        if (response.ok){return response.json();}
+        throw new Error('Request failed.');
+    }).then(function(data){
+        console.log(data.result);
+        
+        htmlSalesRep = salesReport(data);
+        console.log(htmlSalesRep);
+        content = document.getElementById("managerView");
+        content.innerHTML = htmlSalesRep;
+
+    }).catch(function(error) {
+        console.log(error);
+    }
+);}
+
+function salesReport(data){
+    salesTable = '<table><tbody><tr>';
+    salesTable = salesTable + "<th>Item Name</th>";
+    salesTable = salesTable + "<th>Sales</th>";
+    salesTable = salesTable + "<th>Profit</th>";
+    salesTable = salesTable + "</tr>"
+    for (let i = 0; i < data.result.length; i++){
+        salesTable = salesTable + '<tr id = "itemSold">';
+        salesTable = salesTable + "<td>" + data.result[i].item_name + "</td>";
+        salesTable = salesTable + "<td>" + data.result[i].sales + "</td>";
+        salesTable = salesTable + "<td>" + data.result[i].profit + "</td>";
+        salesTable = salesTable + "</tr>"
+    }
+    
+    salesTable = salesTable + '</tbody></table>'
+    return salesTable;
 }
 
-async function restockReport(){
-    alert("restock report");
 
+// Excess Report - Given a timestamp, display the list of items that only sold less than 10% of their inventory between the timestamp and the current time, assuming no restocks have happened during the window.
+const createPopMenuItemReport = document.getElementById("popMenuItemButton");
+
+
+// Restock Report - Display the list of items whose current inventory is less than the item's minimum amount to have around before needing to restock.
+const createExcessReport = document.getElementById("excessReportButton");
+
+// New Seasonal Menu Item - Your vendor just got a new seasonal menu item they have never sold before. Provide and demonstrate the ability to add this menu item to their POS (and any associated inventory items).
+const createRestockReport = document.getElementById("restockReportButton");
+
+// (Teams of 5 Only) What Sales Together - Given a time window, display a list of pairs of menu items that sell together often, popular or not, sorted by most frequent.
+const createSalesTogetherReport = document.getElementById("salesTogetherButton");
+
+function googleTranslateElementInit(){
+    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 }
